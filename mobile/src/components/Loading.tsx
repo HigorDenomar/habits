4d9d5f99b9c6a5176ae8d616dc36a85d3b2742ad
Higor Denomar <higordenomar@gmail.com>
2023-01-17 01:45:20 -0300

    feature(mobile): setup mobile application

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+export function Loading() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#09090A',
+      }}
+    >
+      <ActivityIndicator color='#7C3AED' />
+    </View>
+  );
+}
