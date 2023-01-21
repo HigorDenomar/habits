f791106726db2f8f39b4ca576eb84d4bd0c523f0
Higor Denomar <higordenomar@gmail.com>
2023-01-21 16:43:52 -0300

    feature(web): create checkbox with radix ui

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,8 +1,39 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Sexta-feira',
+  'Sábado',
+];
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleCreateANewHabit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(title, weekDays);
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (!weekDays.includes(weekDay)) {
+      setWeekDays(prevState => [...prevState, weekDay]);
+    } else {
+      setWeekDays(prevState => prevState.filter(day => day !== weekDay));
+    }
+  }
+
   return (
-    <form className='w-full flex flex-col mt-6'>
+    <form
+      onSubmit={handleCreateANewHabit}
+      className='w-full flex flex-col mt-6'
+    >
       <label htmlFor='title' className='font-semibold leading-tight'>
         Qual seu comprometimento?
       </label>
@@ -12,12 +43,32 @@ export function NewHabitForm() {
         id='title'
         placeholder='ex.: Exercícios, dormir bem, e'
         className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400'
+        value={title}
+        onChange={event => setTitle(event.target.value)}
         autoFocus
       />
 
       <label htmlFor='' className='font-semibold leading-tight mt-4'>
         Qual a recorrência?
       </label>
+
+      <div className='mt-6 flex flex-col gap-3'>
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox.Root
+            key={weekDay}
+            className='flex items-center gap-3 group'
+            onCheckedChange={() => handleToggleWeekDay(index)}
+          >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <Checkbox.Indicator>
+                <Check size={20} className='text-white' />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className='text-white leading-tight'>{weekDay}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
 
       <button
         type='submit'
