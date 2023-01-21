fe664f61fee748f6bbdc7bf0c8d949f4cd3b8ad3
Higor Denomar <higordenomar@gmail.com>
2023-01-21 00:20:18 -0300

    feature(mobile): create habit screen

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { View, Text, ScrollView } from 'react-native';
+import { BackButton } from '../components/BackButton';
+import dayjs from 'dayjs';
+import { ProgressBar } from '../components/ProgressBar';
+import { CheckBox } from '../components/CheckBox';
+
+interface RouteParams {
+  date: string;
+}
 
 export function Habit() {
+  const { params } = useRoute();
+  const { date } = params as RouteParams;
+
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format('dddd');
+  const dayAndMonth = parsedDate.format('DD/MM');
+
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
-      <Text>Habit</Text>
+      <BackButton />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <Text className='mt-6 text-zinc-400 font-semibold text-base lowercase'>
+          {dayOfWeek}
+        </Text>
+        <Text className='mt-6 text-white font-extrabold text-3xl lowercase'>
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={40} />
+
+        <View className='mt-6'>
+          <CheckBox title='Dormir 8h' checked={true} />
+          <CheckBox title='Beber 2L de água' checked={true} />
+          <CheckBox title='Caminhar' checked={false} />
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, ScrollView } from 'react-native';
 
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
@@ -12,6 +13,8 @@ const minimumSummaryDaysSize = 12 * 7; // 12 weeks
 const amountOfDaysToFill = minimumSummaryDaysSize - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
@@ -34,7 +37,10 @@ export function Home() {
       >
         <View className='flex-row flex-wrap'>
           {datesFromYearStart.map(date => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate('habit', { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0
