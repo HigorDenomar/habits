047f937e992f58164e50244649ae174ea79578fe
Higor Denomar <higordenomar@gmail.com>
2023-01-21 17:57:27 -0300

    feature(web): send the new habit  to the backend server

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,12 +1,14 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import { FormEvent, useState } from 'react';
+import { api } from '../lib/axios';
 
 const availableWeekDays = [
   'Domingo',
   'Segunda-feira',
   'Terça-feira',
   'Quarta-feira',
+  'Quinta-feira',
   'Sexta-feira',
   'Sábado',
 ];
@@ -15,10 +17,20 @@ export function NewHabitForm() {
   const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
-  function handleCreateANewHabit(event: FormEvent) {
+  async function handleCreateANewHabit(event: FormEvent) {
     event.preventDefault();
 
-    console.log(title, weekDays);
+    if (!title || weekDays.length === 0) return;
+
+    await api.post('/habits', {
+      title,
+      weekDays,
+    });
+
+    setTitle('');
+    setWeekDays([]);
+
+    alert('Hábito criado com sucesso!');
   }
 
   function handleToggleWeekDay(weekDay: number) {
@@ -57,6 +69,7 @@ export function NewHabitForm() {
           <Checkbox.Root
             key={weekDay}
             className='flex items-center gap-3 group'
+            checked={weekDays.includes(index)}
             onCheckedChange={() => handleToggleWeekDay(index)}
           >
             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
