4bcaf7a2a563fa0dcac45a3124d78277d2153dd4
Higor Denomar <higordenomar@gmail.com>
2023-01-22 01:37:32 -0300

    style(web): create animations with react-native-reanimated

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -4,6 +4,7 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
 } from 'react-native';
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
 import clsx from 'clsx';
@@ -21,9 +22,13 @@ export function CheckBox({ title, checked = false, ...props }: CheckBoxProps) {
       {...props}
     >
       {checked ? (
-        <View className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'>
+        <Animated.View
+          className='h-8 w-8 bg-green-500 rounded-lg items-center justify-center'
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
           <Feather name='check' size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
         <View className='h-8 w-8 bg-zinc-900 rounded-lg' />
       )}
